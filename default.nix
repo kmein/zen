3f9ba4b7c7af9b7148813d879346ab9aa05b5e95
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-02-11 21:33:59 +0100

    fix(nix): choose the best™ haskell nix setup

--- default.nix
+++ default.nix
@@ -1,10 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
-let blessings-package = fetchgit {
+haskellPackages.developPackage {
+  root = ./.;
+  source-overrides.blessings = fetchgit {
     url = "https://cgit.krebsco.de/blessings";
     rev = "v2.2.0";
     sha256 = "1pb56dgf3jj2kq3cbbppwzyg3ccgqy9xara62hkjwyxzdx20clk1";
   };
-in haskellPackages.callCabal2nix "zen" ./. {
-  blessings = haskellPackages.callCabal2nix "blessings" blessings-package {};
+  modifier = drv: haskell.lib.addBuildTools drv (with haskellPackages; [
+    cabal-install
+    ghcid
+    (hoogleLocal { packages = drv.propagatedBuildInputs; })
+  ]);
 }
